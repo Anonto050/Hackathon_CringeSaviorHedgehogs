1eca21888b1072cec7f336e38b8095875a9d6fb5
Anonto050 <65712214+Anonto050@users.noreply.github.com>
2022-08-05 08:42:28 +0600

    Add files via upload

--- Student Management System Project In Django/student_management_system/wsgi.py
+++ Student Management System Project In Django/student_management_system/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import signal
+
+import sys
+import traceback
+
+import time
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_management_system.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
